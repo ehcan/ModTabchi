a5c9954f58ac0c09a9062c8838e0b5d316af06ab
ehcan <mr.ehsan.rezaei@gmail.com>
2017-03-17 12:33:30 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,5 +17,5 @@ chmod 777 tabchi-*.sh
 RED='\033[0;31m'
 NC='\033[0m'
 CYAN='\033[0;36m'
-echo -e "${CYAN}Installation Completed! Create a bot with creator.lua (lua creator.lua)${NC}"
+echo -e "${CYAN}Installation Completed! Create a bot with creator.lua (302000159)${NC}"
 exit
